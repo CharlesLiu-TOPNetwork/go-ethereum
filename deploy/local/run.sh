e28e0724d3cc750d4150fc8a2fee1099437b5fad
CharlesLiu <charles.liu@topnetwork.org>
2022-02-25 15:55:08 +0800

    update run.sh sleep

--- deploy/local/run.sh
+++ deploy/local/run.sh
@@ -24,18 +24,21 @@ echo "import default genesis account: "
 ./geth --datadir data/node0 account import keystores/prikey_0.hex --password passwd
 ./geth --datadir data/node1 account import keystores/prikey_1.hex --password passwd
 ./geth --datadir data/node2 account import keystores/prikey_2.hex --password passwd
+sleep 1
 
 # import genesis json:
 echo "import genesis json: "
 ./geth --datadir data/node0 init poa_genesis.json
 ./geth --datadir data/node1 init poa_genesis.json
 ./geth --datadir data/node2 init poa_genesis.json
+sleep 2
 
 # start geth at background:
 echo "start geth at background: "
 ./geth --datadir data/node0 --networkid 616 --port 10010 --nodiscover --allow-insecure-unlock --unlock '0' --password ./passwd --http --http.port 8540 --http.api eth,web3,admin,net,miner,personal --rpc.allow-unprotected-txs --mine --miner.threads 1 --miner.gasprice 100 --syncmode full 2>>data/node0/teth.log &
 ./geth --datadir data/node1 --networkid 616 --port 10011 --nodiscover --allow-insecure-unlock --unlock '0' --password ./passwd --http --http.port 8541 --http.api eth,web3,admin,net,miner,personal --rpc.allow-unprotected-txs --mine --miner.threads 1 --miner.gasprice 100 --syncmode full 2>>data/node1/teth.log &
 ./geth --datadir data/node2 --networkid 616 --port 10012 --nodiscover --allow-insecure-unlock --unlock '0' --password ./passwd --http --http.port 8542 --http.api eth,web3,admin,net,miner,personal --rpc.allow-unprotected-txs --mine --miner.threads 1 --miner.gasprice 100 --syncmode full 2>>data/node2/teth.log &
+sleep 2
 
 # get enode info:
 enode_info0=`./geth attach http://127.0.0.1:8540 --exec "admin.nodeInfo.enode"`
@@ -67,7 +70,7 @@ peers_count2=`./geth attach http://127.0.0.1:8542 --exec "net.peerCount"`
 # ./geth attach http://127.0.0.1:8541 --exec "miner.start(1)"
 # ./geth attach http://127.0.0.1:8542 --exec "miner.start(1)"
 
-sleep 1
+sleep 2
 # init contracts:
 # eth.sendTransaction({from:eth.accounts[0],to:"0000000000000000000000000000000000000100",data:"0xe1c7392a"})
 res=`./geth attach http://127.0.0.1:8540 --exec "eth.sendTransaction({from:eth.accounts[0],to:\"0000000000000000000000000000000000000100\",data:\"0xe1c7392a\",gas:500000})"`
